2b2668f7b83389fa2a29240f8f2631fd9eeed7ce
Mark Ambrocio <16071902+markanator@users.noreply.github.com>
2022-04-19 22:52:55 -0600

    chore(api): :construction_worker: using prisma as ORM

--- apps/api/src/config/db.ts
+++ apps/api/src/config/db.ts
@@ -1,8 +1,10 @@
 import * as mongoose from 'mongoose';
+import { config } from 'dotenv';
+config();
 
 const connectDb = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.DATABASE_URL);
 
     console.log('Connection to mongoDB:', conn.connection.host);
   } catch (error) {
